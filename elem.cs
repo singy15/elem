243b7b3e2904f8c0517d4da7cc19e4394b6984f4
singy15 <uesastorage3@gmail.com>
2019-07-12 21:07:11 +0900

    Improved DI.

--- elem.cs
+++ elem.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
 
 namespace Elem {
   [AttributeUsage(AttributeTargets.Class)]
@@ -22,68 +23,218 @@ namespace Elem {
   [AttributeUsage(AttributeTargets.Class)]
   class Service : Component { }
 
+  [AttributeUsage(AttributeTargets.Class)]
+  class Controllers : Component { }
+
   [AttributeUsage(AttributeTargets.Property)]
-  class Autowired : Attribute { }
+  class Autowired : Attribute { 
+    public string Qualifier { get; set; }
+
+    public Autowired(string qualifier) {
+      Qualifier = qualifier;
+    }
+
+    public Autowired() {
+      Qualifier = null;
+    }
+  }
+
+  [AttributeUsage(AttributeTargets.Property)]
+  class AutowiredGroup : Attribute { 
+    public string GroupName { get; set; }
+
+    public AutowiredGroup(string groupName) {
+      GroupName = groupName;
+    }
+  }
 
   class Context {
-    private HashSet<Type> types = new HashSet<Type>();
+    protected const BindingFlags INJECTION_TARGET = 
+      BindingFlags.InvokeMethod 
+      | BindingFlags.Public 
+      | BindingFlags.NonPublic 
+      | BindingFlags.Instance;
     private Dictionary<Type, object> pool = new Dictionary<Type, object>();
+    private Dictionary<string, Type> beanDef = new Dictionary<string, Type>();
+    private Dictionary<string, object> beanPool = 
+      new Dictionary<string, object>();
+    protected HashSet<Type> types = new HashSet<Type>();
+    protected Dictionary<string, List<string>> beanDefGroup = 
+      new Dictionary<string, List<string>>();
+    public bool AutowireSingleImpl { get; set; }
 
     public Context() {
+      Initialize();
+    }
+
+    public Context(string contextPath) {
+      XDocument xml = XDocument.Load(contextPath);
+      XElement configuration = xml.Element("configuration");
+      var beans = configuration.Elements("beans").Elements("bean");
+      foreach (XElement bean in beans)
+      {
+        string id = bean.Attribute("id").Value;
+        string type = bean.Attribute("type").Value;
+
+        if(beanDef.ContainsKey(id)) {
+          throw new Exception("Duplicate bean definition : " + id);
+        }
+
+        Type cand = Assembly
+          .GetExecutingAssembly()
+          .GetTypes()
+          .Where(x => (x.FullName == type))
+          .SingleOrDefault();
+
+        if(null == cand) {
+          throw new Exception("Bean candidate not found : " + type);
+        }
+
+        beanDef.Add(id, cand);
+      }
+
+      Initialize();
+    }
+
+    public void Initialize() {
+      AutowireSingleImpl = true;
+
       Assembly
         .GetExecutingAssembly()
         .GetTypes()
         .Where(x => (null != x.GetCustomAttribute(typeof(Component))))
         .ToList()
-        .ForEach(x => types.Add(x));
+        .ForEach(x => {
+          if(!beanDef.ContainsKey(x.FullName)) {
+            beanDef.Add(x.FullName, x);
+          }
+        });
+    }
+
+    public virtual object CreateInstance(string beanName) {
+      object obj = (object)Activator.CreateInstance(beanDef[beanName]);
+      InjectBean(obj);
+      InjectBeanGroup(obj);
+      return obj;
+    }
+
+    public void InjectBean(object obj) {
+       obj
+        .GetType()
+        .GetProperties(INJECTION_TARGET)
+        .Where(f => (null != f.GetCustomAttribute(typeof(Autowired))))
+        .ToList()
+        .ForEach(prop => {
+          Autowired autowired = 
+            (Autowired)(prop.GetCustomAttribute(typeof(Autowired)));
+
+          if(null != autowired.Qualifier) {
+            prop.SetValue(obj, GetBean(autowired.Qualifier));
+          } else if(!prop.PropertyType.IsInterface) {
+            prop.SetValue(obj, GetBean(prop.PropertyType.FullName));  
+          } else {
+            if(beanDef.ContainsKey(prop.PropertyType.FullName)) {
+              prop.SetValue(obj, GetBean(prop.PropertyType.FullName));  
+            } else if(AutowireSingleImpl) {
+              List<Type> xs = new List<Type>();
+              foreach (KeyValuePair<string, Type> pair in beanDef)
+              {
+                xs.Add(pair.Value);
+              }
+              IEnumerable<Type> cands = xs
+                .Where(x => 
+                    null != x.GetInterface(prop.PropertyType.FullName));
+
+              if(cands.Count() == 0) {
+                throw new Exception("No compatible bean found : " 
+                    + prop.PropertyType.FullName);
+              }
+              
+              if(cands.Count() > 1) {
+                throw new Exception("Multiple compatible bean found : " 
+                    + prop.PropertyType.FullName);
+              }
+
+              Type cand = cands.Single();
+
+              prop.SetValue(obj, GetBean(cand.FullName));  
+            } else {
+              throw new Exception("Can't inject bean : " 
+                  + prop.PropertyType.FullName);
+            }
+          }
+        });
+    }
+
+    public void InjectBeanGroup(object obj) {
+      obj
+        .GetType()
+        .GetProperties(INJECTION_TARGET)
+        .Where(f => (null != f.GetCustomAttribute(typeof(AutowiredGroup))))
+        .ToList()
+        .ForEach(prop => {
+          AutowiredGroup autowiredGroup = 
+            (AutowiredGroup)(prop.GetCustomAttribute(typeof(AutowiredGroup)));
+
+          if(!beanDefGroup.ContainsKey(autowiredGroup.GroupName)) {
+            throw new Exception("Group not found : " + autowiredGroup.GroupName);
+          }
+
+          List<object> ls = new List<object>();
+          foreach(string bean in beanDefGroup[autowiredGroup.GroupName]) {
+            ls.Add(GetBean(bean));
+          }
+
+          prop.SetValue(obj, ls);
+        });
     }
 
     public object GetBean(Type type) {
-      if(!types.Contains(type)) {
-        throw new Exception("Type not found : " + type.Name);
+      return GetBean(type.FullName);
+    }
+
+    public object GetBean(string beanName) {
+      if(!beanDef.ContainsKey(beanName)) {
+        throw new Exception("Bean definition not found : " + beanName);
       }
 
-      if(pool.ContainsKey(type)) {
-        return pool[type];
+      if(beanPool.ContainsKey(beanName)) {
+        return beanPool[beanName];
       } else {
-        object obj = CreateInstance(type);
-        pool.Add(type, obj);
+        object obj = CreateInstance(beanName);
+        beanPool.Add(beanName, obj);
         return obj;
       }
     }
+  }
 
-    public object CreateInstance(Type type) {
-      object obj = (object)Activator.CreateInstance(type);
+  class WebContext : Context {
+    public WebContext() : base() {
+      DefineControllersGroup();
+    }
 
-      type
-        .GetProperties(
-          BindingFlags.InvokeMethod 
-          | BindingFlags.Public 
-          | BindingFlags.NonPublic 
-          | BindingFlags.Instance)
-        .Where(f => (null != f.GetCustomAttribute(typeof(Autowired))))
-        .ToList()
-        .ForEach(f => f.SetValue(obj, GetBean(f.PropertyType)));
-            
-      return obj;
+    public WebContext(string contextPath) : base(contextPath) {
+      DefineControllersGroup();
+    }
+
+    public void DefineControllersGroup() {
+      beanDefGroup.Add("Elem.Controllers", Assembly.GetExecutingAssembly()
+        .GetTypes()
+        .Where(x => (null != x.GetCustomAttribute(typeof(Controller))))
+        .Select(x => x.FullName)
+        .ToList());
     }
   }
 
+  [Component]
   class Server {
-    private const string ROOT_URL_BASE = "http://*:{0}/";
+    private const string ROOT_URL_BASE = 
+      "http://localhost:{0}/"; // "http://*:{0}/";
     private const string STATIC_ROOT = "./public/";
     private string rootUrl;
-    private List<object> controllers;
 
-    public Server() {
-      Context ctx = new Context();
-
-      this.controllers = Assembly.GetExecutingAssembly()
-        .GetTypes()
-        .Where(x => (null != x.GetCustomAttribute(typeof(Controller))))
-        .Select(x => ctx.GetBean(x))
-        .ToList();
-    }
+    [AutowiredGroup("Elem.Controllers")]
+    public List<object> Controllers { get; set; }
 
     public void Start(int port) {
       Console.WriteLine( "*** elem started on port {0} ***", port);
@@ -101,6 +252,7 @@ namespace Elem {
       }
       catch (Exception ex) {
         Console.WriteLine("Error: " + ex.Message);
+        throw ex;
       }
     }
 
@@ -110,7 +262,7 @@ namespace Elem {
 
       context.Response.StatusCode = 200;
 
-      var ctrlMethod = controllers
+      var ctrlMethod = Controllers
         .SelectMany(c => c.GetType()
                           .GetMethods( BindingFlags.Public 
                                        | BindingFlags.Instance 
@@ -124,7 +276,8 @@ namespace Elem {
         .FirstOrDefault();
 
       if(null != ctrlMethod) {
-        ctrlMethod.MethodInfo.Invoke(ctrlMethod.Ctrl, new object[] { context });
+        ctrlMethod.MethodInfo.Invoke(ctrlMethod.Ctrl, 
+            new object[] { context });
       } else {
         string path = STATIC_ROOT 
           + url.Substring(rootUrl.Length, url.Length - rootUrl.Length);
